ee4c13a5ca38e5ded99a00463095f6c2907cadee
Gabriel Vargas <gabriel.vargas.coelho@gmail.com>
2020-09-03 15:22:18 -0300

    Add project

---
+++ README.md
@@ -0,0 +1,16 @@
+# Blog with Gatsby
+
+Project created to start learning about (Gastby)[https://www.gatsbyjs.com/].
+
+It was made following a tutorial on (freecodecamp)[https://www.freecodecamp.org/], to access the tutorial, (click here)[https://www.freecodecamp.org/news/build-a-developer-blog-from-scratch-with-gatsby-and-mdx/]
+
+## Running it
+1. Clone the repo
+2. Run to install dependecies:
+```cmd
+yarn 
+```
+3. Finally run:
+```cmd
+yarn dev
+```
---
+++ src/pages/index.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { graphql, Link } from 'gatsby';
+import styled from 'styled-components';
+import Img from 'gatsby-image';
+
+// import Dump from '../components/Dump';
+import { Layout } from '../components/Layout';
+
+const IndexWrapper = styled.main``;
+
+const PostWrapper = styled.div``;
+
+const Image = styled(Img)`
+  border-radius: 5px;
+`;
+
+export default ({ data }) => {
+  return (
+    <Layout>
+      {/* <Dump data={data} /> */}
+
+      <IndexWrapper>
+        {data.allMdx.nodes.map(
+          ({ id, excerpt, frontmatter, slug }) => (
+            <PostWrapper key={id}>
+              <Link to={slug}>
+                {!!frontmatter.cover ? (
+                  <Image 
+                    sizes={frontmatter.cover.childImageSharp.sizes}
+                  />
+                ) : null}
+                <h1>{frontmatter.title}</h1>
+                <p>{frontmatter.date}</p>
+                <p>{excerpt}</p>
+              </Link>
+            </PostWrapper>
+        ))}
+      </IndexWrapper>
+    </Layout>
+  );
+}
+
+export const query = graphql`
+  query SITE_INDEX_QUERY {
+    allMdx(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { published: { eq: true } } }
+    ) {
+      nodes {
+        id
+        excerpt(pruneLength: 250)
+        frontmatter {
+          title
+          date(formatString: "YYYY MMMM Do")
+          cover {
+            publicURL
+            childImageSharp {
+              sizes(maxWidth: 2000, traceSVG: { color: "#639" }) {
+                ...GatsbyImageSharpSizes_tracedSVG
+              }
+            }
+          }
+        }
+        slug
+      }
+    }
+  }
+`;
